3e26936a466f29e5adbe0c1d26209a2ba98263b2
jaspertsai <jaspert99@gmail.com>
2014-04-08 09:26:42 -0700

    finish third lesson

--- src/com/example/yamba/StatusActivity.java
+++ src/com/example/yamba/StatusActivity.java
@@ -4,12 +4,9 @@ import android.app.Activity;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
-import android.view.View.OnClickListener;
-import android.widget.Button;
 import android.widget.EditText;
 
-public class StatusActivity extends Activity implements OnClickListener{
-	Button buttonUpdate;
+public class StatusActivity extends Activity {
 	EditText editStatus;
 	
 	/**Called when the activity is first created. */
@@ -18,10 +15,8 @@ public class StatusActivity extends Activity implements OnClickListener{
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.status);
 		
-		buttonUpdate = (Button) findViewById(R.id.button_update);
 		editStatus = (EditText) findViewById(R.id.edit_status) ;
 		
-		buttonUpdate.setOnClickListener(this) ;
 	}
 
 	public void onClick(View v) {
