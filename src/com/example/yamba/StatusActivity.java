09954c8efa81f61aa2203c7511febfdbb4abe321
jaspertsai <jaspert99@gmail.com>
2014-04-08 18:02:55 -0700

    working

--- src/com/example/yamba/StatusActivity.java
+++ src/com/example/yamba/StatusActivity.java
@@ -7,12 +7,16 @@ import android.view.View;
 import android.widget.EditText;
 
 public class StatusActivity extends Activity {
+	static final String TAG = "StatusActivity";
 	EditText editStatus;
 	
 	/**Called when the activity is first created. */
 	@Override
-	protected void onCreate(Bundle savedInstanceState) {
-		super.onCreate(savedInstanceState);
+	protected void onCreate(Bundle bundle) {
+		super.onCreate(bundle);
+		
+		Log.d(TAG, "onCLick with Bundle: " + bundle) ;
+		
 		setContentView(R.layout.status);
 		
 		editStatus = (EditText) findViewById(R.id.edit_status) ;
@@ -22,6 +26,6 @@ public class StatusActivity extends Activity {
 	public void onClick(View v) {
 		String statusText = editStatus.getText().toString();
 		
-		Log.d("StatusActivity", "onClicked!:" + statusText) ;
+		Log.d(TAG, "onClicked!:" + statusText) ;
 	}
 }
